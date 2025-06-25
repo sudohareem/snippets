730c5485ca584bab187283737ef5c6b98f07b57f
sudohareem <k225094@nuedu.pk>
2025-06-25 22:53:05 +0500

    Complete Django Tutorial 3: Views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"  # helps with namespacing in templates
+
 urlpatterns = [
-    path("", views.index, name="index"),
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
