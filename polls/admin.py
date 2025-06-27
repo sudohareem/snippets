596e5758f30f2ec218112b60f31ce83004f20765
sudohareem <k225094@nuedu.pk>
2025-06-27 19:23:32 +0500

    Customize Django admin with inline choices, filters, search (tutorial part 7)

--- polls/admin.py
+++ polls/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-class ChoiceInline(admin.TabularInline):  # Or use StackedInline for vertical layout
+
+class ChoiceInline(admin.TabularInline):  # or admin.StackedInline
     model = Choice
-    extra = 3  # Number of empty choice forms to display
+    extra = 3  # shows 3 empty choice slots
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
