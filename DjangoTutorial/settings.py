b811dab2a987f4ab842e1a9e55556dba9c477e41
sudohareem <k225094@nuedu.pk>
2025-07-02 07:40:36 +0500

    Add snippets app and serializer

--- DjangoTutorial/settings.py
+++ DjangoTutorial/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    #'DjangoTutorial.snippets',
+   'snippets.apps.SnippetsConfig',
+
 ]
 
 MIDDLEWARE = [
