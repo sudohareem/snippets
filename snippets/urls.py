e2fd3966fb0b2b8dc008c3d58fdafb70c44911f4
sudohareem <k225094@nuedu.pk>
2025-07-03 01:48:31 +0500

    part 3 : class based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
 ]
-
