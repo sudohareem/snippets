cadf2864b2978c696cd1ba200b5b4eeed7f56e0e
sudohareem <k225094@nuedu.pk>
2025-07-02 07:51:03 +0500

    Update views, urls, and settings for snippets API

--- DjangoTutorial/urls.py
+++ DjangoTutorial/urls.py
@@ -1,3 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('snippets.urls')),
+]
 """
 URL configuration for DjangoTutorial project.
 
